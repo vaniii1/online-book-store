27393fcf8ad0e75a770296d17148fb8dc3644e16
ivan hromovyi <gromovoy1994@gmail.com>
2023-08-25 14:10:59 +0200

    created dao and service for Book entity

---
+++ src/main/java/com/example/onlinebookstore/model/Book.java
@@ -0,0 +1,24 @@
+package com.example.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    @Column(unique = true)
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
